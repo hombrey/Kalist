e51e32e0912271e14a99dd281a677860b9ad172c
Hombrey <hombrey.escalante@gmail.com>
2015-09-13 11:26:17 +0700

    evolve month view loop structure

--- app/src/main/java/com/archbrey/Kalist/ListModel.java
+++ app/src/main/java/com/archbrey/Kalist/ListModel.java
@@ -37,10 +37,15 @@ public class ListModel {
     private static TextView[] DayofWeekLabel;
 
     private static ArrayList<TextView> TextViewList;
-    private static ArrayList<TextView> EventViewList;
-    private static ArrayList<LinearLayout[]> SubEventView;
-    public static ArrayList<LinearLayout> EventView;
 
+
+    private static ArrayList<LinearLayout[]> SubEventView;
+    private static ArrayList<TextView> SubGroupText;
+    private static ArrayList<ArrayList<TextView>> SubEventList;
+
+    public static ArrayList<LinearLayout> EventView;
+    private static ArrayList<TextView> EventHeaderText;
+    private static ArrayList<TextView> EventViewText;
 
 
 public ListModel(){
@@ -57,8 +62,7 @@ public ListModel(){
     eventParams = new LinearLayout.LayoutParams (
             LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT);
-    eventParams.gravity=Gravity.BOTTOM;
-
+    eventParams.gravity=Gravity.TOP;
 
     MonthLabel = new TextView[12];
     for (int monthInc=0; monthInc<=11; monthInc++) {
@@ -74,7 +78,8 @@ public ListModel(){
 
 
     TextViewList = new ArrayList<>();
-    EventViewList = new ArrayList<>();
+    EventViewText = new ArrayList<>();
+    EventHeaderText = new ArrayList<>();
     EventView = new ArrayList<>();
     SubEventView = new ArrayList<>();
 
@@ -98,63 +103,84 @@ public ListModel(){
 
     public void drawMonthList(){
 
-        lookupStart.set(NavModel.currentYear, NavModel.currentMonth, 1, 0,0);
+        lookupStart.set(NavModel.currentYear, NavModel.currentMonth, 1, 0, 0);
         lookupStop.set(NavModel.currentYear, NavModel.currentMonth + 1, 1, 0, 0);
         fetchList();
 
-        listBox.removeAllViews();
+        listBox.removeAllViews(); //remove contents of layout
         listLayout.removeAllViews();
-        EventViewList.clear();
+        EventHeaderText.clear(); // remove contents of Text view list
+        SubEventView.clear();
+        EventView.clear();
 
         Collections.sort(eventArrayList, new CalStartCompare());
 
-        ArrayList<ArrayList<EventItem>> MonthSocket = new ArrayList<ArrayList<EventItem>>();
-
+        int ViewCount = -1;
+        int SubViewCount = -1;
         int arrayListIndex = 0;
-        MonthSocket.add(new ArrayList<EventItem>()); int socketCount = 0; //dummy position "0" so actual monthsocket will fill up from (1)
+
+        //private static ArrayList<LinearLayout[]> SubEventView;
+        //private static ArrayList<TextView> SubGroupText;
+        //private static ArrayList<ArrayList<TextView>> SubEventList;
+
+        SubEventView.add(new LinearLayout[2]); SubViewCount++;//dummy position "0" so actual event socket will fill up from (1)
         for (int incMonth = 1; incMonth <= NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH); incMonth++) {
 
-            MonthSocket.add(new ArrayList<EventItem>());
             int testDate;
 
-             if (arrayListIndex<eventArrayList.size()) //make sure that arrayListIndex does not access any element beyond eventArrayList size
+            if (arrayListIndex<eventArrayList.size()) {//make sure that arrayListIndex does not access any element beyond eventArrayList size
+                testDate = eventArrayList.get(arrayListIndex).dayOfMonth(true);
+
+            } //if (arrayListIndex<eventArrayList.size())
+            else {
+                incMonth = NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH) + 1; //exit major loop if contents eventArrayList are exhausted
+                testDate = NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH) + 2; //just make sure testDate does not ever match incrementer index
+            } //else of if (arrayListIndex<eventArrayList.size())
+
+            if (testDate == incMonth) { //one time action per subgroup
+
+                //add header
+                if (ViewCount==-1)  addGroupHeaderView("Week "+String.valueOf(eventArrayList.get(arrayListIndex).Week(true)), ++ViewCount);
+                else if (arrayListIndex>0) //check to add to group header only if actual events have been added to the list
+                    if (eventArrayList.get(arrayListIndex).Week(true) != eventArrayList.get(arrayListIndex - 1).Week(true))
+                        addGroupHeaderView("Week " + String.valueOf(eventArrayList.get(arrayListIndex).Week(true)), ++ViewCount);
+
+                EventView.add(new LinearLayout(mainContext)); ViewCount++;
+                EventHeaderText.add(new TextView(mainContext));
+                EventHeaderText.get(ViewCount).setText(" ");
+                EventView.get(ViewCount).setOrientation(LinearLayout.HORIZONTAL);
+
+                SubEventView.add(new LinearLayout[2]); SubViewCount++;
+                SubEventView.get(SubViewCount)[0] = new LinearLayout(mainContext);
+                SubEventView.get(SubViewCount)[1] = new LinearLayout(mainContext);
+                SubEventView.get(SubViewCount)[1].setOrientation(LinearLayout.VERTICAL);
+
+                while  (testDate == incMonth) { //for cases when multiple events match subgroup
+
+                    if (arrayListIndex<eventArrayList.size()) {
                     testDate = eventArrayList.get(arrayListIndex).dayOfMonth(true);
-             else testDate = NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)+1;
 
-               while  (testDate == incMonth) {
-                   MonthSocket.get(incMonth).add(eventArrayList.get(arrayListIndex));
-                   arrayListIndex++;
-                   if (arrayListIndex<eventArrayList.size())
-                       testDate = eventArrayList.get(arrayListIndex).dayOfMonth(true);
-                   else testDate = NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)+1;
+                        if  (testDate == incMonth) { //add to list only if current event item in array matches time socket criteria
+                            EventHeaderText.get(ViewCount).append("\n  (" + String.valueOf(eventArrayList.get(arrayListIndex).dayOfMonth(true)) + ") " +
+                               eventArrayList.get(arrayListIndex).dayOfWeek(true) + " | " +
+                               eventArrayList.get(arrayListIndex).Title +
+                               " ");
+                            arrayListIndex++;
+                        } // if  (testDate == incMonth)
 
-               } //while ( (eventArrayList.get(arrayListIndex).dayOfMonth(true) == incMonth) || (KeepInLoop) )
+                    } //if (arrayListIndex<eventArrayList.size())
+
+                    else testDate = NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)+1;
+
+                } //while ( (eventArrayList.get(arrayListIndex).dayOfMonth(true) == incMonth) || (KeepInLoop) )
+
+                EventView.get(ViewCount).addView(EventHeaderText.get(ViewCount), eventParams);
+                listLayout.addView(EventView.get(ViewCount), eventParams);
+
+            }// if (testDate == incMonth)
 
         } //for (int inc = 0; inc <= NavModel.navMonthCalendar.getActualMaximum(Calendar.DAY_OF_MONTH); inc++)
 
-
-        int ViewCount = -1;
-        int SubViewCount = -1;
-
-        for (int inc = 0; inc < eventArrayList.size(); inc++) {
-            if (inc==0) addGroupHeaderView("\n Week "+String.valueOf(eventArrayList.get(inc).Week(true)), ++ViewCount);
-            else if (eventArrayList.get(inc).Week(true) != eventArrayList.get(inc-1).Week(true))  addGroupHeaderView("Week "+String.valueOf(eventArrayList.get(inc).Week(true)), ++ViewCount);
-
-            SubEventView.add(new LinearLayout[2]); SubViewCount++;
-            SubEventView.get(SubViewCount)[0] = new LinearLayout(mainContext);
-            SubEventView.get(SubViewCount)[1] = new LinearLayout(mainContext);
-
-            EventViewList.add(new TextView(mainContext)); ViewCount++;
-            EventViewList.get(ViewCount).setTextColor(SettingsActivity.textColor);
-            EventViewList.get(ViewCount).setGravity(Gravity.LEFT);
-
-            EventViewList.get(ViewCount).setText("   (" +String.valueOf(eventArrayList.get(inc).dayOfMonth(true)) + ") " +
-                                                eventArrayList.get(inc).dayOfWeek(true) + " | " +
-                                                eventArrayList.get(inc).Title +
-                                                    " ");
-
-            listLayout.addView(EventViewList.get(ViewCount), eventParams);
-        }//for (int inc = 0; inc < eventArrayList.size(); inc++)
 
 
         listBox.addView(listLayout, eventParams); //add assembled Linear layout into scrollview
@@ -164,11 +190,16 @@ public ListModel(){
 
     private void addGroupHeaderView(String GroupHeader,int ViewCount){
 
-        EventViewList.add(new TextView(mainContext));
-        EventViewList.get(ViewCount).setTextColor(SettingsActivity.textColor);
-        EventViewList.get(ViewCount).setGravity(Gravity.LEFT);
-        EventViewList.get(ViewCount).setText(GroupHeader);
-        listLayout.addView(EventViewList.get(ViewCount), eventParams);
+        EventView.add(new LinearLayout(mainContext));
+        EventHeaderText.add(new TextView(mainContext));
+        EventHeaderText.get(ViewCount).setTextColor(SettingsActivity.textColor);
+        EventHeaderText.get(ViewCount).setGravity(Gravity.LEFT);
+        EventHeaderText.get(ViewCount).setText(GroupHeader);
+
+        EventView.get(ViewCount).addView(EventHeaderText.get(ViewCount), eventParams);
+
+        listLayout.addView(EventView.get(ViewCount), eventParams);
+
 
     } //private void addWeekView()
 
@@ -208,7 +239,7 @@ public ListModel(){
 
     public void listDate() {
 
-        lookupStart.set(NavModel.currentYear, NavModel.currentMonth, NavModel.currentDate, 0,0);
+        lookupStart.set(NavModel.currentYear, NavModel.currentMonth, NavModel.currentDate, 0, 0);
         lookupStop.set(NavModel.currentYear, NavModel.currentMonth, NavModel.currentDate+1, 0,0);
 
         fetchList();
